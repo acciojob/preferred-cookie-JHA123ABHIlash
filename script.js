bad310f684fa454424219d28d9c4c545ccf66dfb
Abhilash Kumar Jha <152012438+JHA123ABHIlash@users.noreply.github.com>
2026-06-04 15:56:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const fontsize=document.querySelector('#fontsize');
+const fontcolor=document.querySelector('#fontcolor');
+const form=document.querySelector('form');
+
+form.addEventListener('submit',(e)=>{
+	e.preventDefault();
+	let size=fontsize.value;
+	let color=fontcolor.value;
+
+	document.cookie=`fontsize=${size}`;
+	document.cookie=`fontcolor=${color}`;
+
+	
+})
